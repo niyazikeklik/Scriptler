9ddbe03bcf953d3b4a9ea6d261885378967f53ed
Niyazi Keklik <niyazikeklik@gmail.com>
2021-09-13 15:26:07 +0300

    Kod düzeltmeleri

--- Twitter Unfollow.js
+++ Twitter Unfollow.js
@@ -1,4 +1,4 @@
-var list = [];
+var kontrolEdildi = [];
 var count_kontrol_edilen = 0;
 var count_takipten_cikilan = 0;
 
@@ -6,15 +6,15 @@ function cik() {
     var element = document.querySelectorAll('[data-testid=UserCell]');
     for (var i = 0; i < element.length; i++) {
         var id = element[i].innerText.split('\n')[1];
-        if (id.indexOf('@') == -1) { id = element[i].innerText.split('\n')[0] };
-        if (list.indexOf(id) == -1) {
-            list.push(id);
+        id = id.startsWith('@') ? id : element[i].innerText.split('\n')[0];
+        if (!kontrolEdildi.includes(id)) {
+            kontrolEdildi.push(id);
             count_kontrol_edilen++;
-            if (element[i].innerText.indexOf("Seni takip") == -1) {
+            if (!element[i].innerText.includes("Seni")) {
                 //if (count_takipten_cikilan < 100) { count_takipten_cikilan++; continue; }
-                element[i].querySelector('[aria-label="Takip ediliyor ' + id + '"]').click();
+                element[i].querySelector(`[aria-label="Takip ediliyor ${id}"]`).click();
                 document.querySelectorAll('[data-testid=confirmationSheetConfirm]')[0].click();
-                console.log("Takipten çıkıldı: " + id + " - " + element[i].innerText.split('\n')[0] + ' ' + count_takipten_cikilan + "/" + count_kontrol_edilen);
+                console.log(`Takipten çıkıldı: ${id} - ${element[i].innerText.split('\n')[0]}|${count_takipten_cikilan}/${count_kontrol_edilen}`);
                 count_takipten_cikilan++;
             }
 
@@ -22,10 +22,9 @@ function cik() {
     }
 }
 var onceki = 0;
-
 var idintrvl = setInterval(() => {
     cik();
     window.scrollBy(0, 1000);
-    if (window.scrollY == onceki) clearInterval(idintrvl);
-    onceki = window.scrollY;
+    /*if (window.scrollY == onceki) clearInterval(idintrvl);
+    onceki = window.scrollY;*/
 }, 1500);
