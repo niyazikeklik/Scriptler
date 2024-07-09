d0d8dbf36419d41b73041d33e66f6f5e7abab9da
Niyazi Keklik <niyazikeklik@gmail.com>
2024-07-09 17:32:55 +0300

    Create Twitter Unfollow En.js

---
+++ Twitter Unfollow En.js
@@ -0,0 +1,34 @@
+var r;
+function enabledUnfollow(){
+var kontrolEdildi = [];
+var count_kontrol_edilen = 0;
+var count_takipten_cikilan = 0;
+
+function cik() {
+    var elements = document.querySelectorAll('[data-testid=UserCell]');
+    elements.forEach(element => {
+        var id = element.innerText.split('\n')[1];
+        id = id.startsWith('@') ? id : element.innerText.split('\n')[0];
+        if (!kontrolEdildi.includes(id)) {
+            kontrolEdildi.push(id);
+            count_kontrol_edilen++;
+            if (!element.innerText.includes("Follows you")) {
+                element.querySelector(`[aria-label="Following ${id}"]`).click();
+                setTimeout(()=> {  
+                    document.querySelector('[data-testid=confirmationSheetConfirm]').click();
+                    console.log(`Takipten çıkıldı: ${id} - ${element.innerText.split('\n')[0]}|${count_takipten_cikilan}/${count_kontrol_edilen}`);
+                    count_takipten_cikilan++;
+                });
+            }
+
+        }
+    });
+}
+//var onceki = 0;
+    r = setInterval(() => {
+        cik();
+        window.scrollBy(0, 500);
+    }, 500);
+}
+
+enabledUnfollow();
