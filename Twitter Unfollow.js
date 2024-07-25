fdc3202884413098b29a5869fd62054590e0b36d
Niyazi Keklik <niyazikeklik@gmail.com>
2024-07-25 21:29:32 +0300

    Update Twitter Unfollow.js

--- Twitter Unfollow.js
+++ Twitter Unfollow.js
@@ -12,7 +12,7 @@ function cik() {
         if (!kontrolEdildi.includes(id)) {
             kontrolEdildi.push(id);
             count_kontrol_edilen++;
-            if (!element.innerText.includes("Seni")) {
+            if (!element.innerText.includes("Seni") or 1==1) {
                 element.querySelector(`[aria-label="Takip ediliyor ${id}"]`).click();
                 setTimeout(()=> {  
                     document.querySelector('[data-testid=confirmationSheetConfirm]').click();
